d8b0e91f52c9775d1829c56c13f6bffd9120b219
Giorgio <imatesiu@gmail.com>
2016-01-28 21:58:15 +0100

    enable pin added

--- Firmware/Test/Test.ino
+++ Firmware/Test/Test.ino
@@ -7,17 +7,20 @@
 // defines pins numbers
 const int stepPin = 54; 
 const int dirPin = 55; 
+const int enaPin = 38;
 const int PIN_PSU_POWER = 12;
  
 void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
+  pinMode(enaPin,OUTPUT);
   pinMode(PIN_PSU_POWER, OUTPUT ); 
 }
 void loop() {
-  digitalWrite( PIN_PSU_POWER, LOW ); 
-  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  digitalWrite( PIN_PSU_POWER, LOW );
+  digitalWrite( enaPin, LOW );
+  digitalWrite(dirPin,true); // Enables the motor to move in a particular direction
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH); 
@@ -27,7 +30,7 @@ void loop() {
   }
   delay(1000); // One second delay
   
-  digitalWrite(dirPin,LOW); //Changes the rotations direction
+  digitalWrite(dirPin,false); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
   for(int x = 0; x < 400; x++) {
     digitalWrite(stepPin,HIGH);
