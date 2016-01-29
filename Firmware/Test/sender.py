9e7f94b488fe5b73e4162f662574a874904ffa50
Giorgio <imatesiu@gmail.com>
2016-01-29 20:28:12 +0100

    create sender python

---
+++ Firmware/Test/sender.py
@@ -0,0 +1,38 @@
+import serial
+import sys
+from time import sleep
+
+
+if len(sys.argv)>2:
+	port = sys.argv[2]
+else:
+	port = "COM12"
+print port
+ser = serial.Serial(port,115200,timeout=.9)
+
+
+
+
+fname = sys.argv[1]
+with open(fname) as f:
+	contents = f.readlines()
+	
+x=0
+for content in contents:
+	command = content[:len(content)-1]
+	print command
+	ser.write(str(command))
+	reply = ser.readline()
+	print  str(reply)
+	if str(reply)!='DONE\r\n':
+		while str(reply)!='DONE\r\n':
+			reply = ser.readline()
+			print reply
+			sleep(.1)
+			if x==0:
+				break
+	if x==-1:
+		exit(0)
+	x+=1
+	
+ser.close()
